fa597f8a76e3aa909a83fded06a7914417ab01fe
bulineta <needagadget@gmail.com>
2017-06-07 22:26:06 +0300

    new changes

--- app/src/minSdkIcs/java/com/google/firebase/quickstart/auth/FirebaseUIActivity.java
+++ app/src/minSdkIcs/java/com/google/firebase/quickstart/auth/FirebaseUIActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
