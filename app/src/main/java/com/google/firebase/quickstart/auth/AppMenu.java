c7806eec74eb5f0ed63ea2cf499436ebb1e7bea3
bulineta <needagadget@gmail.com>
2017-06-19 23:47:06 +0300

    test tezt

--- app/src/main/java/com/google/firebase/quickstart/auth/AppMenu.java
+++ app/src/main/java/com/google/firebase/quickstart/auth/AppMenu.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 
 public class AppMenu extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,13 +19,15 @@ public class AppMenu extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+
                     return true;
                 case R.id.navigation_notifications:
-
+                    ProfilePage profilePage = new ProfilePage();
+                    FragmentManager manager = getSupportFragmentManager();
+                    manager.beginTransaction().replace(R.id.contentLayout, profilePage, profilePage.getTag()).commit();
                     return true;
             }
             return false;
@@ -39,7 +40,7 @@ public class AppMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_menu);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
