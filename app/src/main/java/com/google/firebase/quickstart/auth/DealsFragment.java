c49c4c63eac02ccbfa8b43ccb1cd52e573566e56
bulineta <needagadget@gmail.com>
2017-06-20 17:31:58 +0300

    Futu-te-n gura!

--- app/src/main/java/com/google/firebase/quickstart/auth/DealsFragment.java
+++ app/src/main/java/com/google/firebase/quickstart/auth/DealsFragment.java
@@ -20,10 +20,12 @@ public class DealsFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deals, container, false);
+        View view = inflater.inflate(R.layout.testclassfragment, container, false);
+        ImageView imageView = (ImageView) view.findViewById(R.id.my_image);
+        return view;
     }
 
 }
