6f617596d08788c151b76e25af6b99e02ebb3db3
bulineta <needagadget@gmail.com>
2017-06-20 17:03:32 +0300

    Futu-te-n gura!

--- app/src/main/java/com/google/firebase/quickstart/auth/AppMenu.java
+++ app/src/main/java/com/google/firebase/quickstart/auth/AppMenu.java
@@ -3,6 +3,7 @@ package com.google.firebase.quickstart.auth;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -10,39 +11,38 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 public class AppMenu extends AppCompatActivity {
-
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-
-                    return true;
-                case R.id.navigation_dashboard:
-
-                    return true;
-                case R.id.navigation_notifications:
-                    ProfilePage profilePage = new ProfilePage();
-                    FragmentManager manager = getSupportFragmentManager();
-                    manager.beginTransaction().replace(R.id.contentLayout, profilePage, profilePage.getTag()).commit();
-                    return true;
-            }
-            return false;
-        }
-
-    };
-
+    private BottomNavigationView bottomNavigation;
+    private Fragment fragment;
+    private FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_menu);
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.replace(R.id.container, new DashboardFragment());
+        tx.commit();
+        bottomNavigation = (BottomNavigationView)findViewById(R.id.navigation);
+        fragmentManager = getSupportFragmentManager();
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.navigation_home:
+                        fragment = new DashboardFragment();
 
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+                        break;
+                    case R.id.navigation_dashboard:
+                        fragment = new ProfilePage();
+                        break;
+                    case R.id.navigation_notifications:
+                        fragment = new DealsFragment();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.container, fragment).commit();
+                return true;
+            }
+        });
     }
-
 }
--- app/src/main/java/com/google/firebase/quickstart/auth/DealsFragment.java
+++ app/src/main/java/com/google/firebase/quickstart/auth/DealsFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ProfilePage extends Fragment {
+public class DealsFragment extends Fragment {
 
 
-    public ProfilePage() {
+    public DealsFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class ProfilePage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_page, container, false);
+        return inflater.inflate(R.layout.fragment_deals, container, false);
     }
 
 }
